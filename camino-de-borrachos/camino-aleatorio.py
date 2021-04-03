ae66c8cbbb3a1480246f3d3e698c8fee5a61c726
Daniel Fernandez <daniel@synthbiosys.com>
2021-04-03 16:55:45 -0600

    Código funcional con estadísticas para la caminata aleatoria de borrachos a 10, 100 y 1000 pasos

---
+++ camino-de-borrachos/camino-aleatorio.py
@@ -0,0 +1,41 @@
+from borracho import BorrachoTradicional
+from campo import Campo
+from coordenada import Coordenada
+
+def caminata(campo, borracho, pasos):
+    inicio = campo.obtener_coordenada(borracho)
+
+    for _ in range(pasos):
+        campo.mover_borracho(borracho)
+
+    return inicio.distancia(campo.obtener_coordenada(borracho))
+
+def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
+    borracho = tipo_de_borracho(nombre='Daniel')
+    origen = Coordenada(0, 0)
+    distancias = []
+
+    for _ in range(numero_de_intentos): # " _ no usar variable"
+        campo = Campo()
+        campo.anadir_borracho(borracho, origen)
+        simulacion_caminata = caminata(campo, borracho, pasos)
+        distancias.append(round(simulacion_caminata, 1))
+    return distancias
+
+def main (distancias_de_caminata, numero_de_intentos, tipo_de_borracho):
+
+    for pasos in distancias_de_caminata:
+        distancias = simular_caminata(pasos, numero_de_intentos, tipo_de_borracho)
+        distancia_media = round(sum(distancias) / len(distancias), 4)
+        distancia_maxima = max(distancias)
+        distancia_minima = min(distancias)
+        print(f' {tipo_de_borracho.__name__} caminata aleatoria de {pasos} pasos')
+        print(f'Media = {distancia_media}')
+        print(f'Minima = {distancia_minima}')
+        print(f'Maxima = {distancia_maxima}')
+
+if __name__ == '__main__':
+    distancias_de_caminata = [10, 100, 1000]
+    numero_de_intentos = 100
+
+    main(distancias_de_caminata, numero_de_intentos, BorrachoTradicional)
