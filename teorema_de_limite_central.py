1655bcf2971abc98afc55a71a1b5fe168b0b78d6
Daniel Fernández <daniel@synthbiosys.com>
2021-04-23 17:52:06 -0500

    Notas sobre las definiciones de muestreo y teorema de limite central

---
+++ teorema_de_limite_central.py
@@ -0,0 +1,14 @@
+    """
+    Es uno de los teoremas más importantes de la estadística.
+    
+    Nos permite convertir cualquier distribución en una
+    distribución normal.
+    
+    Permite entender cualquier distribución como la distribución
+    normal de sus media y eso nos permite aplicar todo lo que sabemos
+    de distribuciones normales.
+    
+    ¿Cómo trabajar con datos experimentales?
+    
+    
+    """
