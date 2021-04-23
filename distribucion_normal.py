17edca0f78d8bb32772939fb90313fe8d003c516
Daniel Fernández <daniel@synthbiosys.com>
2021-04-22 21:59:40 -0500

    Notas sobre distribucion normal

---
+++ distribucion_normal.py
@@ -0,0 +1,20 @@
+"""
+        DISTRIBUCIÓN NORMAL
+Se define completamente por su media y su
+desviación estándar.
+
+Permite calcular intervalos de confianza con
+la regla empírica.
+
+Una de sus características es su simetría
+respecto de la media.
+   
+   REGLA EMPÍRICA
+   
+También conocida como la regla de 68-95-99.7
+
+Señala cuál es la dispersión de los datos en una
+distribución normal a uno, dos y tres sigmas
+(desviaciones estandar)
+        
+"""
