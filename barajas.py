831fa1ccfacc206a472ee6e27d4eb9d4203dbf94
Daniel Fernández <daniel@synthbiosys.com>
2021-04-22 22:27:44 -0500

    Calculo de probabilidad de obtener un par en un un tamano de mano dado por el usuario

--- barajas.py
+++ barajas.py
@@ -17,6 +17,27 @@ def obtener_mano(barajas, tamano_mano):
     
     return mano
 
+# Probabilidad de encontrar una corrida en una probabilidad de 5 cartas  
+    # El color (flush) o también llamado "corrida" es una combinación de 
+    # cinco cartas del mismo palo (o más), no necesariamente consecutivas.   
+    
+def corrida(manos):
+    for mano in manos:
+        valores = []
+        for carta in mano:
+            valores.append(carta[1])
+            
+    counter = dict(collections.Counter(valores))
+    
+    cinco = 0
+    
+    for val in counter.values():
+        if val == 5:
+            cinco += 1
+            break
+        
+    return(cinco)
+    
 def main(tamano_mano, intentos):
     barajas = crear_baraja()
     
@@ -25,23 +46,13 @@ def main(tamano_mano, intentos):
     for __ in range(intentos):
         mano = obtener_mano(barajas, tamano_mano)
         manos.append(mano)
-
-    pares = 0
-    for mano in manos:
-        valores = []
-        for carta in mano:
-            valores.append(carta[1])
-            
-        counter = dict(collections.Counter(valores))
-        for val in counter.values():
-            if val == 3:
-                pares += 1
-                break
+    
+    flush = corrida(manos)
+    
+    probabilidad = flush/intentos
+    
+    print(f'La probabilidad de obtener una corrida en una mano de tamano {tamano_mano} es {probabilidad}')
         
-        
-    probabilidad_par = pares/intentos
-    print(f'La probabilidad de obtener un par en una mano de {tamano_mano} es {probabilidad_par}')
-
 if __name__ ==  '__main__':
     tamano_de_mano = int(input('De cuantas barajas será la mano: '))
     intentos = int(input('Cuantos intentos de la simulacion: '))
